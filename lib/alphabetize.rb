448ee666c954eb6ac4240f9fbccf0c9fe5278389
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 19:18:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
 def alphabetize(arr)
   # code here
+  max = arr.max_by(&:length).size
+  sorted = arr.sort_by {|word| priority_number(word,max)}
+  return sorted
+end
+
+
+def priority_number (word,max)
+  alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  order = alphabet.split(//)
+  number_array = []
+
+  word.split(//).each do |character|
+    number_array << order.index(character)
+  end
+
+  (max-number_array.size).times do
+    number_array << "0"
+  end
+  return number_array
 end
