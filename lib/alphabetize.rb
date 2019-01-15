fab6cc4e8a4601dea3cfa4fd5d81ab944659f06a
Chris Camacho <cacamacho91@gmail.com>
2019-01-15 14:05:00 +0000

    done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def esp_map
+  esp_map = {}
+  ESPERANTO_ALPHABET.chars.each_with_index do |letter, index|
+    esp_map[letter] = index
+  end
+  esp_map
+end
+
 def alphabetize(arr)
-  # code here
+  arr.sort do |a, b|
+    index = 0
+    while (a[index] == b[index])
+      index += 1
+    end
+    esp_map[a[index]] <=> esp_map[b[index]]
+  end
 end
